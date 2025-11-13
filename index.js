22c7c0e9ea2d93eeb3a07d7859aaea8d444ccedb
eoocc <8668125@gmail.com>
2025-11-13 23:27:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -144,7 +144,7 @@ function handleTrojanConnection(ws, msg) {
     if (msg.length < 58) return false;
     const receivedPasswordHash = msg.slice(0, 56).toString();
     const possiblePasswords = [
-      UUID.replace(/-/g, ''),
+      UUID,
     ];
     
     let matchedPassword = null;
